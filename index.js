2f4996a2ba813f2b56067306435fc87a1a271af5
linuxpalmer01 <52800200+linuxpalmer01@users.noreply.github.com>
2019-07-11 15:37:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
+// CLA Installed
 
 const addHeaders = require('./add-headers');
 const setCacheControl = require('./set-cache-control');
